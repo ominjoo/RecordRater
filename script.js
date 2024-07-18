964454d86e2b3f5f6d013f81eb8bbe39994e277d
ominjoo <o.minjoo14@gmail.com>
2024-07-18 04:02:37 -0700

    page 2 added + tracks listing

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var typed = new Typed(".auto-typed", {
     loop : true,
 });
 
+$("#confirm").hide();
 
 
 
 const getAlbum = async(artist, album) => {
     try {
-        const albumGeneralResponse = await fetch(`https://musicbrainz.org/ws/2/release/?fmt=json&query=release:${encodeURIComponent(album)}%20AND%20artist:${encodeURIComponent(artist)}&status:official&limit=20`, {
+        const albumGeneralResponse = await fetch(`https://musicbrainz.org/ws/2/release/?fmt=json&query=release:${encodeURIComponent(album)}%20AND%20artist:${encodeURIComponent(artist)}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json'
@@ -33,29 +34,51 @@ const getAlbum = async(artist, album) => {
         const coverURL = `https://coverartarchive.org/release/${albumID}/front`;
         const albumName = albumRelease.title;
         const artistName = albumRelease['artist-credit'][0].name;
-        const tracks = albumRelease.media[0]['track-count'];
+        const tracksNum = albumRelease.media[0]['track-count'];
         const date = albumRelease.date;
+        const statusType = albumRelease.status;
         
-        displayAlbumInfo(coverURL, albumName, artistName, tracks, date);
-        
-        
+        displayAlbumInfo(coverURL, albumName, artistName, tracksNum, date, statusType);
+
+
+        const releaseResponse = await fetch(`https://musicbrainz.org/ws/2/release/${albumID}?inc=recordings&fmt=json`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        const releaseData = await releaseResponse.json();
+
+        const tracks = releaseData.media[0]?.tracks
+
+        console.log('Tracks:', tracks);
+
     } catch (error) {
         console.error('Error fetching album:', error);
     }
 };
 
-const displayAlbumInfo = (coverURL, albumName, artistName, tracks, date) => {
+const displayAlbumInfo = (coverURL, albumName, artistName, tracksNum, date, statusType) => {
     $('#albumCover').html(`
         <img src="${coverURL}" alt="" style="max-width: 300px;">
     `);
 
+    if(date != "" && date != null){
     $('#albumName').html(`
         <p>${artistName} - ${albumName} (${date})</p>
     `);
-    
+    } else {
+        $('#albumName').html(`
+        <p>${artistName} - ${albumName}</p>
+    `);
+    }
+
+    $('#status').html(`
+        <p>[${statusType} Release]</p>
+    `);
 
     $('#tracks').html(`
-        <p>Tracks: ${tracks}</p>
+        <p>Tracks: ${tracksNum}</p>
     `);
 
 };
@@ -65,12 +88,16 @@ const displayAlbumInfo = (coverURL, albumName, artistName, tracks, date) => {
 $(document).ready(() => {
     $("#search").click(async () => {
         event.preventDefault();
-        
+        $("#confirm").hide();
         let artist = $('#artistInput').val().trim();
         let album = $('#albumInput').val().trim();
 
+        if(!artist || !album) {
+            alert('Please enter artist and album name.');
+            return;
+        }
+
         try {
-            //$("#confirm").hide();
             $("#search").text("Searching...");
             console.log(artist +" "+ album);
             await getAlbum(artist, album);
@@ -84,7 +111,11 @@ $(document).ready(() => {
     });
 });
 
-
+$(document).ready(() => {
+    $("#confirm").click(async () => {
+        window.location.href = 'page2.html';
+    });
+});
 
 
 
